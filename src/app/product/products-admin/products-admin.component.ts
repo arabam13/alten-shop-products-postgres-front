9e234b79f1aff699269e3f9f0d1933595284291e
araba <araba.mohamed@gmail.com>
2024-05-18 22:43:13 +0200

    creating products-admin page

--- src/app/product/products-admin/products-admin.component.ts
+++ src/app/product/products-admin/products-admin.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from "@angular/core";
+import { ConfirmationService } from "primeng/api";
+import { Observable } from "rxjs";
+import { Product } from "../model/product.model";
+import { ProductService } from "../service/product.service";
 
 @Component({
   selector: "app-products-admin",
   templateUrl: "./products-admin.component.html",
   styleUrls: ["./products-admin.component.scss"],
+  providers: [ConfirmationService],
 })
 export class ProductsAdminComponent implements OnInit {
-  constructor() {}
+  products$: Observable<Product[]>;
+  totalProducts$: Observable<number>;
+  productsPerPage$: Observable<number>;
 
-  ngOnInit(): void {}
+  pageSizeOptions = [10, 20, 30];
+
+  constructor(
+    private productService: ProductService,
+    private confirmationService: ConfirmationService
+  ) {}
+
+  ngOnInit(): void {
+    this.initObservables();
+    this.productService.getProductsFromServer();
+  }
+
+  private initObservables() {
+    this.products$ = this.productService.products$;
+    this.totalProducts$ = this.productService.totalProducts$;
+  }
+
+  handlePageSizeChange(event: { page: number; rows: number }) {
+    // console.log({ event });
+    this.productService.getProductsFromServer(event.page + 1, event.rows);
+  }
 }
