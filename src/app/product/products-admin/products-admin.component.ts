7ef906b8aa8918f7b03bc284d672db84fd98e2e1
araba <araba.mohamed@gmail.com>
2024-05-19 12:24:00 +0200

    adding modal to add product

--- src/app/product/products-admin/products-admin.component.ts
+++ src/app/product/products-admin/products-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ConfirmationService } from "primeng/api";
+import { ConfirmationService, MessageService } from "primeng/api";
 import { Observable } from "rxjs";
 import { Product } from "../model/product.model";
 import { ProductService } from "../service/product.service";
@@ -16,10 +16,17 @@ export class ProductsAdminComponent implements OnInit {
   productsPerPage$: Observable<number>;
 
   pageSizeOptions = [10, 20, 30];
+  productDialog: boolean = false;
+  products!: Product[];
+  product: Pick<Product, "name" | "code"> = { name: "", code: "" };
+  selectedProducts!: Product[] | null;
+
+  submitted: boolean = false;
 
   constructor(
     private productService: ProductService,
-    private confirmationService: ConfirmationService
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -37,4 +44,67 @@ export class ProductsAdminComponent implements OnInit {
     // console.log({ event });
     this.productService.getProductsFromServer(event.page + 1, event.rows);
   }
+
+  openNew() {
+    this.product = { name: "", code: "" };
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  hideDialog() {
+    this.productDialog = false;
+    this.submitted = false;
+  }
+
+  saveProduct() {
+    this.submitted = true;
+
+    if (this.product.name?.trim()) {
+      if (this.product.code) {
+        // this.products[this.findIndexById(this.product.id)] = this.product;
+        this.messageService.add({
+          severity: "success",
+          summary: "Successful",
+          detail: "Product Updated",
+          life: 3000,
+        });
+      } else {
+        // this.product.id = this.createId();
+        // this.product.image = "product-placeholder.svg";
+        // this.products.push(this.product);
+        this.messageService.add({
+          severity: "success",
+          summary: "Successful",
+          detail: "Product Created",
+          life: 3000,
+        });
+      }
+
+      this.products = [...this.products];
+      this.productDialog = false;
+      this.product = { name: "", code: "" };
+    }
+  }
+
+  // findIndexById(id: string): number {
+  //   let index = -1;
+  //   for (let i = 0; i < this.products.length; i++) {
+  //     if (this.products[i].id === id) {
+  //       index = i;
+  //       break;
+  //     }
+  //   }
+
+  //   return index;
+  // }
+
+  // createId(): string {
+  //   let id = "";
+  //   var chars =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  //   for (var i = 0; i < 5; i++) {
+  //     id += chars.charAt(Math.floor(Math.random() * chars.length));
+  //   }
+  //   return id;
+  // }
 }
